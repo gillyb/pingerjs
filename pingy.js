a277814b242dd9e0c1f03dc01d189b697463561d
Gilly Barr <gillyb@gmail.com>
2013-06-26 15:05:03 +0300

    minor

--- pingy.js
+++ pingy.js
@@ -5,4 +5,4 @@ var Pinger = require('./Pinger');
 //Pinger.ping('127.0.0.1').repeat(2000).assert().responseCode(200);
 
 Pinger.ping('www.shopyourway.com', 'localhost').repeat(3000).responseCode(200);
-Pinger.ping('127.0.0.1').repeat(5000).assert().regex('<body id="product-page">');
+Pinger.ping('127.0.0.1').repeat(5000).regex('<body id="product-page">');
