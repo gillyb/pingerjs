d0143f537196277b033c953becc6f878404617a5
Gilly Barr <gillyb@gmail.com>
2013-06-26 12:02:43 +0300

    fixing spaces

--- Pinger.js
+++ Pinger.js
@@ -1,4 +1,3 @@
-
 // Pinger
 
 var http = require('http');
@@ -6,84 +5,84 @@ var util = require('util');
 
 exports.ping = function(ip, name) {
 
-	return new Pinger(ip, name);
+    return new Pinger(ip, name);
 
 };
 
 function Pinger(ip, name) {
 
-	var _timeoutId;
-	var _pingResponse, _pingStatusCode, _pingReponseHeaders;
-	var _pingSuccess = true;
-	var _responseAssertions = [];
-
-	var self = this;
-
-	self.ping = function() {
-		console.log('pinging : ' + ip + ' (' + name + ')');
-
-		var pingOptions = {	host: ip };
-		http.request(pingOptions, function(response) {
-			var chunks = [], length = 0;
-
-			response.on('data', function(chunk) {
-            	chunks.push(chunk);
-            	length += chunk.length;
-	        });
-
-	        response.on('end', function() {
-	        	_pingStatusCode = response.statusCode;
-	        	_pingReponseHeaders = response.headers;
-	            _pingResponse = combineChunks(chunks, length).toString();
-	            _responseAssertions.forEach(function(element) {
-	            	element[0](element[1]); // invokes the assertion method
-	            });
-	        });
-
-		}).end();
-
-		return self;
-	};
-
-	self.repeat = function(interval) {
-		var _timeoutId = setInterval(self.ping, interval);
-		return self;
-	};
-
-	self.responseCode = function(expectedCode) {
-		_responseAssertions.push([assertResponseCode, expectedCode]);
-		return self;
-	};
-
-	self.regex = function(expectedRegex) {
-		_responseAssertions.push([assertRegex, expectedRegex]);
-		return self;
-	};
-
-	var assertResponseCode = function(expectedCode) {
-		if (_pingStatusCode != expectedCode)
-			pingFailed();
-		else
-			pingSuccess();
-	};
-
-	var assertRegex = function(regex) {
-		var regex = util.isRegExp(expectedRegex) ? expectedRegex : new RegExp(expectedRegex);
-		if (!regex.test(_pingResponse))
-			pingFailed();
-		else
-			pingSuccess();
-	};
-
-	var pingFailed = function() {
-		_pingSuccess = false;
-		console.log('ping to ' + ip + ' (' + name + ') failed');
-	};
-	var pingSuccess = function() {
-		console.log('ping to ' + ip + ' (' + name + ') success');
-	};
-
-	self.ping();
+    var _timeoutId;
+    var _pingResponse, _pingStatusCode, _pingReponseHeaders;
+    var _pingSuccess = true;
+    var _responseAssertions = [];
+    
+    var self = this;
+    
+    self.ping = function() {
+        console.log('pinging : ' + ip + ' (' + name + ')');
+        
+        var pingOptions = {	host: ip };
+        http.request(pingOptions, function(response) {
+            var chunks = [], length = 0;
+        
+            response.on('data', function(chunk) {
+                chunks.push(chunk);
+                length += chunk.length;
+            });
+        
+            response.on('end', function() {
+                _pingStatusCode = response.statusCode;
+                _pingReponseHeaders = response.headers;
+                _pingResponse = combineChunks(chunks, length).toString();
+                _responseAssertions.forEach(function(element) {
+                    element[0](element[1]); // invokes the assertion method
+                });
+            });
+            
+        }).end();
+            
+        return self;
+    };
+    
+    self.repeat = function(interval) {
+        var _timeoutId = setInterval(self.ping, interval);
+        return self;
+    };
+    
+    self.responseCode = function(expectedCode) {
+        _responseAssertions.push([assertResponseCode, expectedCode]);
+        return self;
+    };
+    
+    self.regex = function(expectedRegex) {
+        _responseAssertions.push([assertRegex, expectedRegex]);
+        return self;
+    };
+    
+    var assertResponseCode = function(expectedCode) {
+        if (_pingStatusCode != expectedCode)
+            pingFailed();
+        else
+            pingSuccess();
+    };
+    
+    var assertRegex = function(regex) {
+        var regex = util.isRegExp(expectedRegex) ? expectedRegex : new RegExp(expectedRegex);
+        if (!regex.test(_pingResponse))
+            pingFailed();
+        else
+            pingSuccess();
+    };
+    
+    var pingFailed = function() {
+        _pingSuccess = false;
+        console.log('ping to ' + ip + ' (' + name + ') failed');
+    };
+    var pingSuccess = function() {
+        console.log('ping to ' + ip + ' (' + name + ') success');
+    };
+    
+    self.ping();
 
 }
 
