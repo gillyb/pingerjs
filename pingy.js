94e1ab9ee9c45c9f48cb145410f262fd57ecbe11
Gilly Barr <gillyb@gmail.com>
2013-06-26 15:04:46 +0300

    minor

--- pingy.js
+++ pingy.js
@@ -1,10 +1,8 @@
-
 // ping utility for environments
 
 var Pinger = require('./Pinger');
 
 //Pinger.ping('127.0.0.1').repeat(2000).assert().responseCode(200);
 
-//Pinger.ping('127.0.0.1').repeat(5000).assert().regex('<body id="product-page">');
-
 Pinger.ping('www.shopyourway.com', 'localhost').repeat(3000).responseCode(200);
+Pinger.ping('127.0.0.1').repeat(5000).assert().regex('<body id="product-page">');
