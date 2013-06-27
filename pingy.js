958a137b128ad23e37e9058dc46d6ecebd471f76
gilly <gilly@delver.com>
2013-06-27 13:38:30 +0300

    adding reporting to the mongodb schema

--- pingy.js
+++ pingy.js
@@ -1,7 +1,37 @@
 // ping utility for environments
 
 var Pinger = require('./Pinger');
+var mongoose = require('mongoose');
 
-Pinger.ping('www.shopyourway.com', 'localhost').repeat(3000).responseCode(200).on('failure', function() { console.log('prod failure'); }).on('success', function() { console.log('prod success' + this.host); });
+mongoose.connect('mongodb://localhost/pingyjs');
+var pingSchema = mongoose.Schema({
+	host: String,
+	environment: String,
+	timestamp: { type:Date, default:Date.now },
+	status: Boolean
+});
+var PingSchema = mongoose.model('Ping', pingSchema);
+
+function reportPingResult(status, host, name) {
+	var pingStatus = new PingSchema({ 
+		host: host,
+		environment: name,
+		status: status=='success' ? true : false
+	});
+	pingStatus.save(function(err) {
+		console.log('Error saving ping status');
+		console.log(err);
+	});
+}
+
+Pinger.ping('www.shopyourway.com', 'localhost')
+	.repeat(3000)
+	.responseCode(200)
+	.on('failure', function() {
+		reportPingResult('failure', this.host, this.name);
+	})
+	.on('success', function() {
+		reportPingResult('success', this.host, this.name);
+	});
 
 Pinger.ping('uat.shopyourway.com', 'uat').repeat(2000).responseCode(200).on('failure', function() { console.log('uat failure'); }).on('success', function() { console.log('uat success'); });
